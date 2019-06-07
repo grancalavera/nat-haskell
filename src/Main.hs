0410be04efc38376082afd25862812934002dcfd
Leon Coto <leon.coto-Rodriguez@weareadaptive.com>
2019-06-07 17:16:58 +0100

    add plus and times

--- src/Main.hs
+++ src/Main.hs
@@ -15,9 +15,22 @@ five = Succ four
 
 toInt :: Nat -> Int
 toInt Zero = 0
-toInt (Succ n) = 1 + toInt n
+toInt (Succ x) = 1 + toInt x
 
 fromInt :: Int -> Maybe Nat
 fromInt i
   | i < 0 = Nothing
-  | otherwise = Just $ foldr (const Succ) Zero [0..(i-1)]
+  | otherwise = Just $ fromInt' i
+  where
+    fromInt' 0 = Zero
+    fromInt' i' = Succ $ fromInt' (i' - 1)
+
+plus :: Nat -> Nat -> Nat
+plus x Zero = x
+plus Zero x = x
+plus (Succ x) y = Succ (plus x y)
+
+times :: Nat -> Nat -> Nat
+times _ Zero = Zero
+times Zero _ = Zero
+times (Succ x) z@(Succ y) = Succ (y `plus` (x `times` z))
