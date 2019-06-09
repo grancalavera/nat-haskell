a0a72e8ab5110ba6b269d433a8cc969449341bc4
Leon Coto <leoncoto@gmail.com>
2019-06-09 10:42:13 +0100

    autoformat code

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 module Main where
-import Prelude  hiding (subtract)
+import           Prelude                           hiding ( subtract )
 
 main :: IO ()
 main = do
@@ -13,9 +13,9 @@ main = do
 data Nat = Zero | Succ Nat deriving (Show, Eq)
 
 instance Ord Nat where
-  compare Zero Zero = EQ
-  compare Zero _ = LT
-  compare _ Zero = GT
+  compare Zero     Zero     = EQ
+  compare Zero     _        = LT
+  compare _        Zero     = GT
   compare (Succ x) (Succ y) = compare x y
 
 zero = Zero
@@ -26,29 +26,28 @@ four = Succ three
 five = Succ four
 
 toInt :: Nat -> Int
-toInt Zero = 0
+toInt Zero     = 0
 toInt (Succ x) = 1 + toInt x
 
 fromInt :: Int -> Maybe Nat
-fromInt i
-  | i < 0 = Nothing
-  | otherwise = Just $ fromInt' i
-  where
-    fromInt' 0 = Zero
-    fromInt' i' = Succ $ fromInt' (i' - 1)
+fromInt i | i < 0     = Nothing
+          | otherwise = Just $ fromInt' i
+ where
+  fromInt' 0  = Zero
+  fromInt' i' = Succ $ fromInt' (i' - 1)
 
 plus :: Nat -> Nat -> Nat
-plus x Zero = x
-plus Zero x = x
-plus (Succ x) y = Succ (plus x y)
+plus x        Zero = x
+plus Zero     x    = x
+plus (Succ x) y    = Succ (plus x y)
 
 times :: Nat -> Nat -> Nat
-times _ Zero = Zero
-times Zero _ = Zero
+times _        Zero       = Zero
+times Zero     _          = Zero
 times (Succ x) z@(Succ y) = Succ (y `plus` (x `times` z))
 
 subtract :: Nat -> Nat -> Maybe Nat
-subtract Zero Zero = Just Zero
-subtract Zero _ = Nothing
-subtract succ Zero = Just succ
+subtract Zero     Zero     = Just Zero
+subtract Zero     _        = Nothing
+subtract succ     Zero     = Just succ
 subtract (Succ x) (Succ y) = subtract x y
